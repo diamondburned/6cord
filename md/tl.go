eb970b9f08e9ec1f6356d4d25b5340c1219593cd
diamondburned (Forefront) <datutbrus@gmail.com>
2019-02-28 04:44:53 -0800

    optimizations, CI changs

--- md/tl.go
+++ md/tl.go
@@ -24,30 +24,14 @@ func isFormatEnter(e bool, p string) string {
 
 func fixQuotes(s string) string {
 	lines := strings.Split(s, "\n")
-	if len(lines) < 1 {
-		return s
-	}
-
-	rebuild := []string{lines[0]}
 
 	for i := 1; i < len(lines); i++ {
-		if len(lines[i]) < 1 {
-			goto Done
+		if (len(lines[i]) > 0 && len(lines[i]) > 0) &&
+			lines[i-1][0] == '>' &&	lines[i][0] != '>' {
+
+			lines[i] += "\n" + lines[i]
 		}
-
-		if lines[i][0] != '>' {
-			if len(lines[i-1]) < 1 {
-				goto Done
-			}
-
-			if lines[i-1][0] == '>' {
-				rebuild = append(rebuild, "")
-			}
-		}
-
-	Done:
-		rebuild = append(rebuild, lines[i])
 	}
 
-	return strings.Join(rebuild, "\n")
+	return strings.Join(lines, "\n")
 }
