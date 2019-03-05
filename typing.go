6e3da27b0a0c6f92e35b91f4868c9e4d15db4c07
diamondburned (Forefront) <datutbrus@gmail.com>
2019-03-04 16:29:25 -0800

    reduced typingstart duration

--- typing.go
+++ typing.go
@@ -3,7 +3,7 @@ package main
 import "time"
 
 var (
-	typingDelay = time.Duration(time.Second * 2)
+	typingDelay = time.Duration(time.Second * 8)
 	typingTimer = time.NewTimer(typingDelay)
 )
 
@@ -11,6 +11,10 @@ func typingTrigger() {
 	select {
 	case <-typingTimer.C:
 		if TriggerTyping {
+			if ChannelID == 0 {
+				return
+			}
+
 			go func() {
 				err := d.ChannelTyping(ChannelID)
 				if err != nil {
