d8659c9551d4f94006f2eee1893b913b558b7c8b
Will Eccles <ghostyx123@gmail.com>
2019-04-29 12:50:23 -0400

    mutexed render queue

--- messagerenderer.go
+++ messagerenderer.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 
 	"github.com/diamondburned/discordgo"
 )
 
 var (
 	messageRender = make(chan *discordgo.Message, 3)
+	msgRenderLock sync.Mutex
 )
 
 // Function takes in a messageCreate buffer
 // do NOT use for update+delete (yet)
 func messageRenderer() {
 	var lastmsg *discordgo.Message
+	msgRenderLock.Lock()
+	defer msgRenderLock.Unlock()
 
 	for m := range messageRender {
 		if getLastAuthor() != m.Author.ID || (lastmsg != nil && messageisOld(m, lastmsg)) {
@@ -32,7 +36,7 @@ func messageRenderer() {
 				color, username,
 				sentTime.Local().Format(time.Stamp),
 			)
-
+			
 			messagesView.Write([]byte(msg))
 			messageStore = append(messageStore, msg)
 		}
