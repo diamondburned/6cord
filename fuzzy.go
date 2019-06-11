7f57fab291cf5f8b042b34a5de3009ec6dc3cd03
diamondburned (Forefront) <datutbrus@gmail.com>
2019-06-10 21:06:45 -0700

    fixed bug

--- fuzzy.go
+++ fuzzy.go
@@ -100,13 +100,5 @@ func autocompHandler(ev *tcell.EventKey) *tcell.EventKey {
 	return nil
 }
 
-// fuck you you fucking tview dev
-// you could just fucking globalize the goddamn selection
-// function, but no. you didn't. why the fuck didn't you?
-// are you fucking retarded in the head? stop trying to
-// handle everything by you yourself and your shitty shoddy
-// little functions, you fucking stupid asshat
-// bloody fucking jesus i fucking hate doing this, but
-// this is literally my only fucking choice
 var autofillfunc func(i int)
 var onhoverfn func(i int)
